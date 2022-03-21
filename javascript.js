c1e8dcddd6414ad1bb88e3780b3278b272cb524d
iamEvgen <evgen.alexa@gmail.com>
2022-03-21 18:24:53 +0300

    ready calc

--- javascript.js
+++ javascript.js
@@ -4,69 +4,11 @@ const firstLine = document.getElementById('firstLine');
 const secondLine = document.getElementById('secondLine');
 
 const dataForCalculator = {
-    firstOperand: '',
-    secondOperand: '',
-    operand: null,
-    result: null,
-    state: 'wait1', // 'wait1', 'wait2'
-    symbol: '',
-    point: true
-};
-
-[...buttons].forEach((element => element.addEventListener('click', findButton)));
-
-function operate(a, b, operator) {
-    
-}
-
-function findButton(event) {
-    let command = event.path[0].id;
-    console.log(command);
-    checkClear(command);
-    checkDel(command)
-    checkNumber(command); 
-}
-
-function checkClear(command) {
-    if (command === 'clear') {
-        dataForCalculator.firstOperand = '';
-        dataForCalculator.secondOperand = '';
-        dataForCalculator.operand = null;
-        dataForCalculator.result = null;
-        dataForCalculator.state = 'wait1';
-        dataForCalculator.symbol = '',
-        dataForCalculator.point = true;
-        firstLine.textContent = ''
-        secondLine.textContent = '0'
-    }
-}
-
-function checkDel(command) {
-    if (command === 'del') {
-        if (dataForCalculator.firstOperand.length <= 1) {
-            dataForCalculator.firstOperand = ''
-            secondLine.textContent = 0;
-        } else {
-            dataForCalculator.firstOperand = dataForCalculator.firstOperand.slice(0, dataForCalculator.firstOperand.length - 1)
-            secondLine.textContent = dataForCalculator.firstOperand;
-        }
-    }
-
-}
-
-function checkNumber(command) {
-    let numbers = {zero: '0', one: '1', two: '2', three: '3', four: '4', five: '5', six: '6', seven: '7', eight: '8', nine: '9'};
-    if (secondLine.textContent.length >= 15) return;
-    if (command in numbers && dataForCalculator.state === 'wait1') {
-        if (!(dataForCalculator.firstOperand === '' && command === 'zero')) {
-            dataForCalculator.firstOperand += numbers[command];
-            secondLine.textContent = dataForCalculator.firstOperand;
-        }
-    }
-}
-
-function checkOperand(command) {
-    let operands = {
+    previousOperand: '',
+    currentOperand: '',
+    operator: null,
+    symbol: {plus: '+', minus: '-', multiply: '*', divide: '/'},
+    operations: {
         plus: (a, b) => a + b,
         minus: (a, b) => a - b,
         multiply: (a, b) => a * b,
@@ -74,11 +16,198 @@ function checkOperand(command) {
             if (b == 0) return 'ERROR';
             return a / b;
         }
+    },
+    history: {
+        status: false,
+        previousOperand: '',
+        currentOperand: '',
+        operator: null
     }
-    let symbols = {plus: '+', minus: '-', multiply: '*', divide: '/'}
-    if (command in operands) {
-        dataForCalculator.operand = operands[command];
-        dataForCalculator.state = 'wait2';
+};
 
+[...buttons].forEach((element => element.addEventListener('click', findButton)));
+
+function findButton(event) {
+    let command = event.path[0].id;
+    checkError();
+    checkClear(command);
+    checkDel(command);
+    checkNumber(command);
+    checkOperator(command);
+    checkResult(command);
+    checkSquareRoot(command);
+    changeSign(command);
+    addPoint(command);
+    if (command !== 'result') clearHisrory();
+};
+
+function checkError() {
+    if (secondLine.textContent === 'ERROR') {
+        checkClear('clear');
     }
-}
+};
+
+function checkClear(command) {
+    if (command === 'clear') {
+        dataForCalculator.previousOperand = '';
+        dataForCalculator.currentOperand = '';
+        dataForCalculator.point = true;
+        dataForCalculator.operator = null;
+        firstLine.textContent = '';
+        updateSecondLine('0');
+    }
+};
+
+function checkDel(command) {
+    if (command === 'del') {
+        if (dataForCalculator.currentOperand.length <= 1) {
+            dataForCalculator.currentOperand = '';
+            updateSecondLine('0');
+        } else {
+            dataForCalculator.currentOperand = dataForCalculator.currentOperand.slice(0, dataForCalculator.currentOperand.length - 1);
+            updateSecondLine(dataForCalculator.currentOperand);
+        }
+    }
+
+};
+
+function checkNumber(command) {
+    let numbers = {zero: '0', one: '1', two: '2', three: '3', four: '4', five: '5', six: '6', seven: '7', eight: '8', nine: '9'};
+    if (secondLine.textContent.length >= 14) return;
+    if (command in numbers) {
+        if (!(dataForCalculator.currentOperand === '' && command === 'zero')) {
+            dataForCalculator.currentOperand += numbers[command];
+            updateSecondLine(dataForCalculator.currentOperand);
+        }
+    }
+};
+
+function ignoreOperator() {
+    if (dataForCalculator.previousOperand === '' && 
+    dataForCalculator.currentOperand === '' &&
+    secondLine.textContent === '0') {
+        return true;
+    }
+};
+
+function normalCount(command) {
+    if (dataForCalculator.previousOperand === '' &&
+    dataForCalculator.currentOperand !== '' &&
+    dataForCalculator.operator === null) {
+        dataForCalculator.previousOperand = dataForCalculator.currentOperand;
+        dataForCalculator.currentOperand = '';
+        dataForCalculator.operator = command;
+        firstLine.textContent = `${dataForCalculator.previousOperand} ${dataForCalculator.symbol[dataForCalculator.operator]}`;
+        updateSecondLine('');
+        return true;
+    }
+};
+
+function changeOperator(command) {
+    if (dataForCalculator.previousOperand !== '' &&
+    dataForCalculator.currentOperand === '' &&
+    dataForCalculator.operator !== null) {
+        dataForCalculator.operator = command;
+        firstLine.textContent = `${dataForCalculator.previousOperand} ${dataForCalculator.operator}`;
+        return true;
+    }
+};
+
+function continueTheCalculation(command) {
+    if (dataForCalculator.previousOperand !== '' &&
+    dataForCalculator.currentOperand !== '' && 
+    dataForCalculator.operator !== null) {
+        let result = operate(dataForCalculator.previousOperand, dataForCalculator.currentOperand, dataForCalculator.operator)
+        dataForCalculator.previousOperand = result;
+        dataForCalculator.currentOperand = '';
+        dataForCalculator.operator = command;
+        firstLine.textContent = `${dataForCalculator.previousOperand} ${dataForCalculator.symbol[dataForCalculator.operator]}`;
+        updateSecondLine('');
+        return true;
+    }
+};
+
+function checkOperator(command) {
+    if (command in dataForCalculator.symbol) {
+        if (ignoreOperator()) return;
+        if (normalCount(command)) return;
+        if (changeOperator(command)) return;
+        if (continueTheCalculation(command)) return;
+    }
+};
+
+function operate(a, b, operator) {
+    return dataForCalculator.operations[operator](+a, +b);
+};
+
+function writeHistory(result) {
+    dataForCalculator.history.status = true;
+    dataForCalculator.history.previousOperand = result;
+    dataForCalculator.history.currentOperand = dataForCalculator.currentOperand;
+    dataForCalculator.history.operator = dataForCalculator.operator;
+};
+
+function clearHisrory() {
+    dataForCalculator.history.status = false;
+    dataForCalculator.history.previousOperand = '';
+    dataForCalculator.history.currentOperand = '';
+    dataForCalculator.history.operator = null;
+};
+
+function checkResult(command) {
+    if (command === 'result' && dataForCalculator.history.status) {
+        let result = operate(dataForCalculator.history.previousOperand, dataForCalculator.history.currentOperand, dataForCalculator.history.operator);
+        firstLine.textContent = `${dataForCalculator.history.previousOperand} ${dataForCalculator.symbol[dataForCalculator.history.operator]} ${dataForCalculator.history.currentOperand} =`;
+        updateSecondLine(result);
+        dataForCalculator.history.previousOperand = result;
+        dataForCalculator.currentOperand = secondLine.textContent;
+    } else if (command === 'result' &&
+    dataForCalculator.previousOperand !== '' &&
+    dataForCalculator.currentOperand !== '' &&
+    dataForCalculator.operator !== null) {
+        let result = operate(dataForCalculator.previousOperand, dataForCalculator.currentOperand, dataForCalculator.operator)
+        firstLine.textContent = `${dataForCalculator.previousOperand} ${dataForCalculator.symbol[dataForCalculator.operator]} ${dataForCalculator.currentOperand} =`;
+        updateSecondLine(result);
+
+        writeHistory(result);
+
+        dataForCalculator.previousOperand = '';
+        dataForCalculator.currentOperand = result;
+        dataForCalculator.operator = null;
+    } else return;
+};
+
+function checkSquareRoot(command) {
+    if (command === 'squareRoot') {
+        dataForCalculator.currentOperand = dataForCalculator.currentOperand ** 0.5;
+        if (isNaN(dataForCalculator.currentOperand)) {
+            updateSecondLine('ERROR');
+        } else {
+            updateSecondLine(dataForCalculator.currentOperand);
+        }
+    }
+};
+
+function changeSign(command) {
+    if (command === 'changeSign') {
+        dataForCalculator.currentOperand = dataForCalculator.currentOperand * -1;
+        updateSecondLine(dataForCalculator.currentOperand);
+    }
+};
+
+function addPoint(command) {
+    if (command === 'point') {
+        if (!secondLine.textContent.includes('.') && secondLine.textContent.length <= 12) {
+            dataForCalculator.currentOperand += '.';
+            updateSecondLine(dataForCalculator.currentOperand);
+        }
+    }
+};
+
+function updateSecondLine(result) {
+    if (result.toString().length <= 15) {
+        secondLine.textContent = result;
+    } else {
+        secondLine.textContent = result.toExponential(9);
+    }
+};
